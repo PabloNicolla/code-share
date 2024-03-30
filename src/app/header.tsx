ea60abeef9d6485ff9c76a40338977cc13229deb
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-03-30 14:16:14 -0400

    Add dependencies and components for displaying rooms on the home page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,20 +3,74 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogInIcon, LogOutIcon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Image from "next/image";
+import Link from "next/link";
 
-export function Header() {
+function AccountDropdown() {
   const session = useSession();
 
+  const isLoggedIn = !!session.data;
+
   return (
-    <header>
-      <div>
-        {session.data ? (
-          <Button onClick={() => signOut()}>Sign Out</Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"link"}>
+          <Avatar className="mr-2">
+            <AvatarImage src={session.data?.user?.image ?? ""} />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+
+          {session.data?.user?.name}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuSeparator />
+        {isLoggedIn ? (
+          <DropdownMenuItem onClick={() => signOut()}>
+            <LogOutIcon className="mr-2" />
+            Sign Out
+          </DropdownMenuItem>
         ) : (
-          <Button onClick={() => signIn("google")}>Sign In</Button>
+          <DropdownMenuItem onClick={() => signIn("google")}>
+            <LogInIcon className="mr-2" /> Sign In
+          </DropdownMenuItem>
         )}
-        {session.data?.user?.name}
-        <ModeToggle />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export function Header() {
+  return (
+    <header className="container mx-auto bg-gray-100 dark:bg-gray-900">
+      <div className="flex items-center justify-between p-2">
+        <Link
+          href="/"
+          className="flex items-center gap-2 text-xl hover:text-blue-400 hover:underline"
+        >
+          <Image
+            src={"/code-share-logo.png"}
+            width={60}
+            height={60}
+            alt="The web application logo"
+          />
+          Code Share
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <AccountDropdown />
+          <ModeToggle />
+        </div>
       </div>
     </header>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,62 @@
-import { db } from "@/db";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Room } from "@/db/schema";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { getRooms } from "@/data-access/rooms";
+
+function RoomCard({ room }: { room: Room }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{room.name}</CardTitle>
+        <CardDescription>{room.description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {room.githubRepo && (
+          <Link
+            href={room.githubRepo}
+            className="flex items-center gap-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <GitHubLogoIcon /> GitHub Project
+          </Link>
+        )}
+      </CardContent>
+      <CardFooter>
+        <Button asChild>
+          <Link href={`/rooms/${room.id}`}> Join Room</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
 
 export default async function Home() {
-  const rooms = await db.query.room.findMany();
+  const rooms = await getRooms();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {rooms.map((room) => {
-        return <div key={room.id}> {room.name} </div>;
-      })}
+    <main className="container min-h-screen p-16">
+      <div className="mb-8 flex items-center justify-between">
+        <h1 className="text-4xl">Find Rooms</h1>
+        <Button asChild>
+          <Link href={"/create-room"}>Create Room</Link>
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4">
+        {rooms.map((room) => {
+          return <RoomCard key={room.id} room={room} />;
+        })}
+      </div>
     </main>
   );
 }
