febd9abf50d300d48e6b20f7d3c69c1084f67f5d
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-04-03 23:02:56 -0400

    Fixed search bar on browse page

--- src/app/browse/search-bar.tsx
+++ src/app/browse/search-bar.tsx
@@ -39,9 +39,9 @@ export function SearchBar() {
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     if (values.search) {
-      router.push(`/?search=${values.search}`);
+      router.push(`/browse/?search=${values.search}`);
     } else {
-      router.push("/");
+      router.push("/browse");
     }
   }
 
@@ -74,7 +74,7 @@ export function SearchBar() {
             variant="link"
             onClick={() => {
               form.setValue("search", "");
-              router.push("/");
+              router.push("/browse");
             }}
           >
             Clear
